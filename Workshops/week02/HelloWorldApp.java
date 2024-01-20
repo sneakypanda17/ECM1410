b42120f7abc181e9a0526ec32366793b93b8143e
sneakypanda17 <danjloveless@gmail.com>
2024-01-20 14:36:48 +0000

    created HelloWorldApp.java

---
+++ Workshops/week02/HelloWorldApp.java
@@ -0,0 +1,8 @@
+// HelloWorld application
+// sneakypanda17 70093595
+// 20/01/2024
+public class HelloWorldApp {
+    public static void main(String[] args) {
+    System.out.println("Hello World!");
+    }
+    }
